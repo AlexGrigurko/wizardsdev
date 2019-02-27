250e45f56bbff02d553ef1c3e0297cd50b7721a0
alex <alexgrigurko@ukr.net>
2019-02-27 20:19:20 +0200

    Home Work 1

--- src/main/java/com/wizardsdev/Main.java
+++ src/main/java/com/wizardsdev/Main.java
@@ -10,7 +10,7 @@ public class Main {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
-        driver.quit()
+        driver.quit();
 
     }
 }
